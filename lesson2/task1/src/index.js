9828653cfc4500cbf3b556c5a84bb96ef7f11e3c
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-08-17 20:07:40 +0300

    test

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -1,61 +1,61 @@
-// import { createStore } from 'redux';
-
-// export const incrementAction = {
-//     type:'INCREMENT',
-// }
-
-// export const decrementAction = {
-//     type:'DECREMENT',
-// };
-
-
-// const counterReducer = (state = 0, action) => {
-//     switch (action.type){
-//         case 'INCREMENT':
-//             return state + 1;
-//         case 'DECREMENT':
-//             return state - 1;
-//         default:
-//             return state; 
-//     }
-// }
-
-// const store = createStore(counterReducer);
-
-// console.log(store.getState())
-// store.dispatch(incrementAction)
-// store.dispatch(incrementAction)
-// store.dispatch(decrementAction)
-
-// console.log(store.getState)
-
-
-
 import { createStore } from 'redux';
 
-const INCREMENT='COUNTER/INCREMENT'
-const DECREMENT='COUNTER/DECREMENT'
-export const increment = () => {
-    return {
-        type: INCREMENT,
-    }
+export const incrementAction = {
+    type:'INCREMENT',
+}
+
+export const decrementAction = {
+    type:'DECREMENT',
 };
-export const decrement = () => {
-    return {
-        type: DECREMENT,
-    }
-};
+
+
 const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-        case INCREMENT:
+    switch (action.type){
+        case 'INCREMENT':
             return state + 1;
-        case DECREMENT:
+        case 'DECREMENT':
             return state - 1;
         default:
-            return state;
+            return state; 
     }
-};
-export const store = createStore(counterReducer);
+}
+
+const store = createStore(counterReducer);
+
+console.log(store.getState())
+store.dispatch(incrementAction)
+store.dispatch(incrementAction)
+store.dispatch(decrementAction)
+
+console.log(store.getState())
+
+
+
+// import { createStore } from 'redux';
+
+// const INCREMENT='COUNTER/INCREMENT'
+// const DECREMENT='COUNTER/DECREMENT'
+// export const increment = () => {
+//     return {
+//         type: INCREMENT,
+//     }
+// };
+// export const decrement = () => {
+//     return {
+//         type: DECREMENT,
+//     }
+// };
+// const counterReducer = (state = 0, action) => {
+//     switch (action.type) {
+//         case INCREMENT:
+//             return state + 1;
+//         case DECREMENT:
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// };
+// export const store = createStore(counterReducer);
 
 // console.log(store.getState())
 // store.dispatch(increment())
---
+++ lesson2/task2/src/index.js
@@ -0,0 +1,29 @@
+import './index.scss';
+import store, { increment, decrement, reset } from './store.js'
+
+const resultElem = document.querySelector(".counter__result");
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+const onIcnrement = () => {
+    store.dispatch(increment())
+}
+const onDecrement = () => {
+    store.dispatch(decrement())
+}
+
+const onReset = () => {
+    store.dispatch(reset())
+}
+
+incrementBtn.addEventListener('click', onIcnrement);
+decrementBtn.addEventListener('click', onDecrement);
+resetBtn.addEventListener('click', onReset);
+
+store.subscribe(() => {
+    const state = store.getState();
+    const currentValue = state.history.reduce((acc, value) => acc+ value, 0);
+    const historyString = state.history.join(' ');
+    resultElem.textContent = `${historyString} = ${currentValue}`;
+})
