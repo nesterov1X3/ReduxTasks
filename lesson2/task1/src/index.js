5454d2d024207e420f1f87aa140ca3745b3dc4bc
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-08-17 18:43:41 +0300

    test

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -1,33 +1,65 @@
+// import { createStore } from 'redux';
+
+// export const incrementAction = {
+//     type:'INCREMENT',
+// }
+
+// export const decrementAction = {
+//     type:'DECREMENT',
+// };
+
+
+// const counterReducer = (state = 0, action) => {
+//     switch (action.type){
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
+//         default:
+//             return state; 
+//     }
+// }
+
+// const store = createStore(counterReducer);
+
+// console.log(store.getState())
+// store.dispatch(incrementAction)
+// store.dispatch(incrementAction)
+// store.dispatch(decrementAction)
+
+// console.log(store.getState)
+
+
+
 import { createStore } from 'redux';
 
-const incrementAction = {
-    type:'INCREMENT',
-}
-
-const decrementAction = {
-    type:'DECREMENT',
+// const INCREMENT='COUNTER/INCREMENT'
+// const DECREMENT='COUNTER/DECREMENT'
+export const increment = () => {
+    return {
+        type: 'INCREMENT',
+    }
 };
-
-
+export const decrement = () => {
+    return {
+        type: 'DECREMENT',
+    }
+};
 const counterReducer = (state = 0, action) => {
-    switch (action.type){
+    switch (action.type) {
         case 'INCREMENT':
             return state + 1;
         case 'DECREMENT':
             return state - 1;
         default:
-            return state; 
+            return state;
     }
-}
+};
+export const store = createStore(counterReducer);
 
-const store = createStore(counterReducer);
+// console.log(store.getState())
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(increment())
 
-console.log(store.getState())
-store.dispatch(incrementAction)
-store.dispatch(incrementAction)
-store.dispatch(decrementAction)
-
-console.log(store.getState)
-
-
-
+// console.log(store.getState())
