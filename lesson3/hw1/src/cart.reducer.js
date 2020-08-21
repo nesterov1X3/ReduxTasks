eb6bc4fd12ae4851a96ba46092868a025971c661
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-08-21 15:11:45 +0300

    test

--- lesson3/hw1/src/cart.reducer.js
+++ lesson3/hw1/src/cart.reducer.js
@@ -1,7 +1,7 @@
 import { ADD_PRODUCT, REMOVE_PRODUCT } from './cart.actions.js'
 
 const initialState = {
-    productList: []
+    products: []
 }
 
 const productReducer = (state = initialState, action) => {
@@ -9,16 +9,16 @@ const productReducer = (state = initialState, action) => {
         case ADD_PRODUCT: {
             return {
                 ...state,
-                productList: state.productList.concat(action.payload.productData)
+                products: state.products.concat(action.payload.productData)
             }
         };
         case REMOVE_PRODUCT: {
-            const newList = state.productList.filter(
+            const newList = state.products.filter(
                 user => user.id !== action.payload.productId
             )
             return {
                 ...state,
-                productList: newList
+                products: newList
             };
 
         }
